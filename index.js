e0602655ec99d7c1be03894d25c1ca84f32c8e79
Abhiraj Bhosle <92145377+Abhiraj721@users.noreply.github.com>
2022-12-21 15:57:40 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ var vol_wf=0.2;
 var checkrain=0;
 var checkwf=0;
 var checknature=0;
+var gifcount=0;
 document.querySelector(".playbtn").innerHTML="<img src='https://app.lofi.co/icons/new/play.svg' >"
 var x=new Audio("/beats/beat"+0+".mp3");
 var a=0;
 var temp=0;
+let mixer_visi=0;
+let scenes_visi=0;
 let details = navigator.userAgent;
 let regexp = /android|iphone|kindle|ipad/i;
 let isMobileDevice = regexp.test(details);
+var gif_arr=["https://i.gifer.com/Mf08.gif","https://i.gifer.com/xK.gif","https://i.gifer.com/fyrV.gif","https://i.gifer.com/7wkh.gif","https://i.gifer.com/EdfH.gif","https://i.gifer.com/2qQQ.gif","https://i.gifer.com/fyrS.gif",]
 if(temp==0)gettime()
 x.addEventListener("ended", function() {
     console.log("um")
@@ -22,6 +26,38 @@ x.addEventListener("ended", function() {
     x=new Audio("/beats/beat"+num+".mp3");
     x.play()
 });
+function scenes_menu(element){
+  console.log(element)
+ document.querySelector("body").style.backgroundImage="url("+element+")"
+ Hide(Bar)
+
+}
+var gif_arr=["https://i.gifer.com/Mf08.gif","https://i.gifer.com/xK.gif","https://i.gifer.com/fyrS.gif","https://i.gifer.com/58yR.gif"]
+for (let i = 1; i < 10; i++) {
+  document.getElementById("gifs").innerHTML+="<div class='col col-lg-4 col-sm-6' id='scenes_id'><img id='scenes01' onClick='scenes_menu(this.src)' class='scenes_gif' src='/bg/img"+i+".gif' ></div> "
+ 
+  
+}
+document.getElementById("Bar").style.visibility="hidden";
+document.getElementById("mydiv").style.visibility="hidden";
+function changegif(){
+  var gif_arr=["https://i.gifer.com/Mf08.gif","https://i.gifer.com/xK.gif","https://i.gifer.com/fyrS.gif","https://i.gifer.com/58yR.gif"]
+  document.body.style.backgroundImage="url("+gif_arr[gifcount]+")"
+  console.log(gifcount)
+  gifcount++;
+}
+document.querySelector("body").style.fontFamily="'Silkscreen', cursive";
+function Hide(HideID) 
+{
+  HideID.style.visibility = "hidden"; 
+  if(HideID.id=="mydiv")mixer_visi--;
+  else scenes_visi--;
+}
+function scenes(){
+scenes_visi++;
+if(scenes_visi%2==1)document.querySelector(".scenes").style.visibility="visible";
+else document.querySelector(".scenes").style.visibility="hidden";
+}
 function gettime(){
   temp++;
   var date=new Date();
@@ -76,20 +112,26 @@ var t=new Audio("/beats/waterfall.webm");
     }
     else t.play()
  }
+ function mixer(){
+  mixer_visi++;
+  console.log(mixer_visi)
+  if(mixer_visi%2==1)document.getElementById("mydiv").style.visibility="visible"
+  else document.getElementById("mydiv").style.visibility="hidden"
+ }
  function wfvolume(val){
      vol_wf=val/250
     t.volume=val/250
  }
 function changebeatback(){
     console.log("im ahere")
-    if(num<0)num=10
+    if(num<0)num=17
     else num--
     x.pause()
     x=new Audio("/beats/beat"+num+".mp3");
     x.play()
 }
 function changebeatforw(){
-    if(num>10)num=1
+    if(num>17)num=1
     else num++
     x.pause()
     x=new Audio("/beats/beat"+num+".mp3");
